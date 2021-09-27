24298f1148f152a854c8fb5446f9eca8c4466043
Nathan Danziger <NateMakesGames@gmail.com>
2021-09-27 00:58:13 -0400

    Added spring joint example, a tether ball pole, to Interaction Objects scene.

---
+++ Snowman Destroyer/Assets/_Snowman Destroyer/Examples/1. Interaction Objects/Rope.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public LineRenderer lineRend;
+    public Transform poleTopTransform;
+    public Transform ballTransform;
+
+    private void Update()
+    {
+        lineRend.SetPosition(0, poleTopTransform.position);
+        lineRend.SetPosition(1, ballTransform.position);
+    }
+}
---
+++ Snowman Destroyer/Assets/_Snowman Destroyer/Examples/1. Interaction Objects/TetherBallPole.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetherBallPole : MonoBehaviour
+{
+    public Transform poleTopTransform;
+    void LateUpdate()
+    {
+        transform.position = poleTopTransform.position;
+        transform.localRotation = Quaternion.identity;
+    }
+}
