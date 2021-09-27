db637868b39e7c0a18b3cc8077e8c9895071e332
Nathan Danziger <NateMakesGames@gmail.com>
2021-09-26 23:08:38 -0400

    Created "Examples" folder, including an "Interaction Objects" scene that explores basic interaction objects and joints.

---
+++ Snowman Destroyer/Assets/_Snowman Destroyer/Examples/1. Interaction Objects/Gem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+public class Gem : MonoBehaviour
+{
+    void OnJointBreak(float breakForce)
+    {
+        GetComponent<InteractionBehaviour>().enabled = true;
+    }
+}
