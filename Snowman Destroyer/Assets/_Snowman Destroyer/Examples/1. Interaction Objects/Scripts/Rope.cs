b0bd601d9b0e3da9ea739c21ba5e351db9562f6f
Nathan Danziger <NateMakesGames@gmail.com>
2021-10-02 02:12:11 -0400

    Brought the code style of custom scripts up to standard. Reorganized some files and objects related to the Interaction Objects scene.

---
+++ Snowman Destroyer/Assets/_Snowman Destroyer/Examples/1. Interaction Objects/Scripts/Rope.cs
@@ -0,0 +1,35 @@
+﻿/*--------------------------------------
+	Nate Danziger Proprietary
+
+	© 2021
+
+	Not licensed for 3rd party use
+---------------------------------------*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NateVR
+{
+    /// <summary>
+	/// Draws a line between the poleTopTransform and
+    /// the ballTransform at the end of every frame. This visually
+    /// represents a rope for the tetherball.
+	/// </summary>
+    public class Rope : MonoBehaviour
+    {
+        [SerializeField]
+        private LineRenderer lineRend;
+        [SerializeField]
+        private Transform poleTopTransform;
+        [SerializeField]
+        private Transform ballTransform;
+
+        private void LateUpdate()
+        {
+            lineRend.SetPosition(0, poleTopTransform.position);
+            lineRend.SetPosition(1, ballTransform.position);
+        }
+    }
+}
---
+++ Snowman Destroyer/Assets/_Snowman Destroyer/Examples/1. Interaction Objects/Scripts/TetherBallPole.cs
@@ -0,0 +1,32 @@
+﻿/*--------------------------------------
+	Nate Danziger Proprietary
+
+	© 2021
+
+	Not licensed for 3rd party use
+---------------------------------------*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NateVR
+{
+    /// <summary>
+	/// Ensures that the TetherBallPole is always
+    /// rotated vertically no matter how the "pole top" is oriented.
+    /// This is designed to ensure the smoothest possible movement
+    /// while maintaining the pole's orientation.
+	/// </summary>
+    public class TetherBallPole : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform poleTopTransform;
+
+        private void LateUpdate()
+        {
+            transform.position = poleTopTransform.position;
+            transform.localRotation = Quaternion.identity;
+        }
+    }
+}
